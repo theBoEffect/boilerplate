1f9631f1ecc9029c35610c895dabb3acab62383e
Bo Motlagh <bo@unitedeffects.com>
2022-10-06 20:31:58 -0400

    fixing default product mapping for permissions

--- src/permissions.js
+++ src/permissions.js
@@ -12,25 +12,47 @@ const api = {
         try {
             let access = req.authInfo;
             // Let root folks through....
-            if (access?.group === config.CORE_EOS_PLATFORM_ID && access.client_credential !== true) return next();
+            if (access?.group === config.CORE_EOS_ROOT_ID && access.client_credential !== true) return next();
             if(!req.orgContext) throw Boom.forbidden('No context for permissions');
             // check that permission is not a url
             // if x-access-url is present.... (only on jwts...)
             if(access?.['x-access-url']) {
                 access = await getPermissions(access?.['x-access-url'], req.authInfo?.token);
             }
+            /**
+             * ATTENTION - YOU MAY NEED TO MODIFY THIS CODE
+             * At this point, you need to check permissions against a product. You can map one specifically and query it here
+             * from your DB or config, or you can just use the default as the code currently does
+             *
+             * if you want to set up a config, you could define products as...
+             * products = config.YOUR_PRODUCT_ARRAY
+             */
+            const products = req.group?.core?.products; //grabbing default products from UE CORE
+            if(!products?.length) throw Boom.forbidden();
             const productAccess = access?.['x-access-products']?.[req.orgContext].split(' ').filter((a) => {
-                return (a.includes(p.simpleStreamProductId));
+                const found = products.filter((pr) => {
+                    const b = a.split(',');
+                    return (pr === b[0]);
+                })
+                return (found.length);
             });
             if(!productAccess?.length) throw Boom.forbidden('No product access');
-            const coded = productAccess[0].split(',')?.[1];
+            const findCodes = productAccess.filter((p) => {
+                const c = p.split(',');
+                return (c.length === 2);
+            })
+            if(!findCodes.length) throw Boom.forbidden();
+            // for now, just use the first option. Another way would be to loop through the list
+            const coded = findCodes[0].split(',')?.[1];
             if(!coded) throw Boom.forbidden('No roles assigned');
+            //console.info(access?.['x-access-permissions']?.[req.orgContext].split(' '));
             const permissions = access?.['x-access-permissions']?.[req.orgContext].split(' ').filter((perm) => {
                 return (perm.includes(`${coded}:::`));
             });
             req.allPermissions = permissions;
             if(!permissions.length) throw Boom.forbidden('You do not have permissions');
             req.userPermissions = await mapper(req, coded, permissions);
+            console.info(req.userPermissions);
             req.enforceOwner = false;
             const owner = req.userPermissions.filter((p) => {
                 return (p.includes(':own'));
